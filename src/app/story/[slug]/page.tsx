bd497f3c8d18f74cc7fca85a9e5f1e5977013552
OrigamiStarz <40199724+OrigamiStarz@users.noreply.github.com>
2025-03-21 02:14:16 -0700

    Fix build issues

--- src/app/story/[slug]/page.tsx
+++ src/app/story/[slug]/page.tsx
@@ -1,26 +1,18 @@
-import fs from "fs";
-import path from "path";
-import Link from "next/link";
-import Image from "next/image";
-import imageIcon from "../../../../public/image.svg";
+import fs from 'fs';
+import path from 'path';
+import Link from 'next/link';
+import Image from 'next/image';
+import imageIcon from '../../../../public/image.svg';
+import React from 'react';
 
-// Function to generate static paths
-export async function generateStaticParams() {
-  const storiesDirectory = path.join(process.cwd(), "stories_data");
-  const filenames = fs.readdirSync(storiesDirectory);
+type Props = {
+  params: Promise<{ slug: string }>;
+};
 
-  return filenames.map((filename) => ({
-    slug: filename.replace(".json", ""),
-  }));
-}
+export default async function StoryPage({ params }: Props) {
+  const { slug } = await params;
 
-// Dynamic Page Component
-export default async function StoryPage({ params }) {
-  const filePath = await path.join(
-    process.cwd(),
-    "stories_data",
-    `${params.slug}.json`,
-  );
+  const filePath = path.join(process.cwd(), 'stories_data', `${slug}.json`);
 
   if (!fs.existsSync(filePath)) {
     return (
@@ -30,13 +22,13 @@ export default async function StoryPage({ params }) {
     );
   }
 
-  const fileContent = JSON.parse(fs.readFileSync(filePath, "utf8"));
+  const fileContent = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 
-  const addLineBreaks = (text: string): JSX.Element[] => {
-    return text.split("\n").map((part, index) => (
+  const addLineBreaks = (text: string): React.JSX.Element[] => {
+    return text.split('\n').map((part, index) => (
       <span key={index}>
         {part}
-        {index < text.split("\n").length - 1 && (
+        {index < text.split('\n').length - 1 && (
           <>
             <br />
             <br />
@@ -48,7 +40,6 @@ export default async function StoryPage({ params }) {
 
   return (
     <div className="bg-neutral-100 dark:bg-neutral-800 m-auto w-7/12 h-[64rem] p-8 rounded-lg shadow-lg text-white overflow-y-scroll max-h-screen">
-      {/* Back Button */}
       <Link
         href="/stories"
         className="mt-4 text-neutral-600 underline hover:text-neutral-600 hover:scale-105 hover:opacity-90 transition"
@@ -56,12 +47,10 @@ export default async function StoryPage({ params }) {
         ← Back
       </Link>
 
-      {/* Title */}
       <h1 className="text-3xl font-bold my-2 text-center text-foreground">
         {fileContent.title}
       </h1>
 
-      {/* Author */}
       <p className="text-neutral-400 italic text-center my-2">
         By {fileContent.author}
       </p>
@@ -69,10 +58,8 @@ export default async function StoryPage({ params }) {
       <hr className="text-gray-500" />
 
       <div className="flex h-full w-full mb-24">
-        {/* Left Side */}
-        {/* Image */}
         <div className="min-w-1/2 h-full m-4 flex justify-center">
-          {fileContent.images && fileContent.images.length > 0 && (
+          {fileContent.images && fileContent.images.length > 0 ? (
             <Image
               src={fileContent.images[0]}
               alt="Image"
@@ -80,30 +67,26 @@ export default async function StoryPage({ params }) {
               height={400}
               className="rounded-lg h-full w-full object-cover"
             />
+          ) : (
+            <div className="flex justify-center items-center bg-neutral-200 dark:bg-neutral-700 rounded-lg h-full w-full">
+              <Image
+                src={imageIcon}
+                alt="Image"
+                width={300}
+                height={300}
+                className="invert-50 w-24 h-24"
+              />
+            </div>
           )}
-          {!fileContent.images ||
-            (fileContent.images.length == 0 && (
-              <div className="flex justify-center items-center bg-neutral-200 dark:bg-neutral-700 rounded-lg h-full w-full">
-                <Image
-                  src={imageIcon}
-                  alt="Image"
-                  width={300}
-                  height={300}
-                  className="invert-50 w-24 h-24"
-                />
-              </div>
-            ))}
         </div>
-        {/* Right Side */}
         <div>
-          {/* Content */}
           <p className="text-foreground my-4">
             {addLineBreaks(fileContent.content)}
           </p>
-          {/* Source (Optional) */}
+
           {fileContent.source && (
             <p className="mt-2 font-[eczar] font-semibold py-8">
-              Source:{" "}
+              Source:{' '}
               <a
                 href={fileContent.source}
                 target="_blank"
